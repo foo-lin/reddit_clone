4479d2ecf75b6046ecbd6bc26c3041cbddaf3046
sugi <santanu2481995@gmail.com>
2020-01-06 23:27:37 +0530

    Added mongoose and connection to mongoDB Atlas

--- server.js
+++ server.js
@@ -3,9 +3,27 @@ const dotenv = require('dotenv');
 
 //Configure env variables
 dotenv.config({ path: './config.env' });
+const mongoose = require('mongoose');
+
+const db = process.env.DATABASE.replace(
+	'<password>',
+	process.env.DATABASE_PASSWORD
+);
+
+mongoose
+	.connect(db, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+		useUnifiedTopology: true
+	})
+	.then(() => {
+		console.log('Connected To DB');
+	});
 
 const app = require('./app');
 
-app.listen(process.env.PORT, () => {
-	console.log(`App running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+	console.log(`App running on port ${PORT}`);
 });
